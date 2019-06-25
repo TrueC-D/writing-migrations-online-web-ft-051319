adc5f73513ee9644f9fc4e92c7f6dda540990e32
Alethea Cary <aletheacary@gmail.com>
2019-06-25 23:04:24 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,9 +1,7 @@
 class StudentDetails < ActiveRecord::Migration[5.1]
   
   def change
-    add_column :students, :grade, :integerer
-      t.integer :grade
-      t.string :birthdate
+    add_column (:students, :grade, :integer), (:students, :birthdate, :string)
     end
   end
   
