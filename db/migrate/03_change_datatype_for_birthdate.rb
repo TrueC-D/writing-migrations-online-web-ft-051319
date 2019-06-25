0373b86290c767417656acc7fd2184a2899d9fda
Alethea Cary <aletheacary@gmail.com>
2019-06-25 23:07:17 +0000

    Automatically backed up by Learn

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,16 @@
+class StudentDetails < ActiveRecord::Migration[5.1]
+  
+  def change
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
+  end
+  
+end
+
+# class StudentDetails < ActiveRecord::Migration[5.1]
+  
+#   def change
+#     change_column(:students, :birthdate, datetime)
+#   end
+  
+# end
